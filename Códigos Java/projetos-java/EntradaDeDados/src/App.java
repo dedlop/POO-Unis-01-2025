cdf5eb46f79b18ee902c03df8afa50e4ec89f92e
dedlop <104914772+dedlop@users.noreply.github.com>
2025-04-21 20:00:39 -0300

    Códigos Java
    Adicionado os códigos Java utilizados nos slides.

---
+++ Códigos Java/projetos-java/EntradaDeDados/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; // Programa utiliza a classe Scanner
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // cria uma Scanner para obter entrada da janela de comando
+        Scanner input = new Scanner(System.in);
+
+        int number1;  // primeiro número a adicionar
+        int number2;  // segundo número a adicionar
+        int sum;  // soma de number1 e number2
+
+        System.out.print("Digite o primeiro número: "); // prompt
+        number1 = input.nextInt(); // lê o primeiro número fornecido pelo usuário
+
+        System.out.print("Digite o segundo número: "); // prompt
+        number2 = input.nextInt();  // lê o segundo número fornecido pelo usuário
+
+        sum = number1 + number2; // soma os números, depois armazena o total em sum
+
+        System.out.printf("A soma é %d\n", sum); // exibe a soma
+
+    }
+}
---
+++ Códigos Java/projetos-java/MeuPrograma/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Olá turma de ADS 2025!!!");
+
+        Empregado kratos = new Empregado();
+        kratos.codigo = 1;
+        kratos.nome = "Kratos Lopes";
+        kratos.salario = 50000;
+
+        kratos.Informacoes();
+
+    }
+}
